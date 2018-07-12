28187b5c2d4756576807e27cd4bb6785f9f431aa
Richard Millet <remillet@yahoo.com>
2018-07-11 17:43:09 -0700

    NOJIRA: Second commit.  Includes files generated by Yeoman generator-cspace-ui-plugin.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/* global cspaceUIPluginProfileOHC */
+/* The cspaceUIPluginProfileOHC global variable is set by webpack (in non-test builds). See
+ * webpack.config.js. */
+
+import { defineMessages } from 'react-intl';
+import messages from './messages';
+import plugins from './plugins';
+import styles from '../styles/cspace-ui-plugin-profile/ohc.css';
+
+module.exports = () => ({
+  messages,
+  className: styles.common,
+  prettyUrls: true,
+  tenantId: '1885',
+  pluginInfo: {
+    cspaceUIPluginProfileOHC: {
+      messages: defineMessages({
+        name: {
+          id: 'cspaceUIPluginProfileOHC.name',
+          defaultMessage: 'OHC profile',
+        },
+      }),
+      version: cspaceUIPluginProfileOHC.packageVersion,
+    },
+  },
+  plugins: plugins.map(plugin => plugin()),
+});
