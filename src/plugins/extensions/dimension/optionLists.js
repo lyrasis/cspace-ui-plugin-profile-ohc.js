cd50b175467ef8d234f63e570e6b358bfe63508d
Brian Harrington <brian.harrington@lyrasis.org>
2018-08-29 12:04:27 -0400

    More changes for OHC

---
+++ src/plugins/extensions/dimension/optionLists.js
@@ -0,0 +1,85 @@
+import { defineMessages } from 'react-intl';
+
+export default {
+  measurementUnits: {
+    values: [
+      'carats',
+      'centimeters',
+      'cubic-centimeters',
+      'feet',
+      'grams',
+      'inches',
+      'kilograms',
+      'liters',
+      'millimeters',
+      'meters',
+      'minutes',
+      'pixels',
+      'pounds',
+      'square-feet',
+      'stories',
+    ],
+    messages: defineMessages({
+      carats: {
+        id: 'option.measurementUnits.carats',
+        defaultMessage: 'carats',
+      },
+      centimeters: {
+        id: 'option.measurementUnits.centimeters',
+        defaultMessage: 'centimeters',
+      },
+      'cubic-centimeters': {
+        id: 'option.measurementUnits.cubic-centimeters',
+        defaultMessage: 'cubic centimeters',
+      },
+      feet: {
+        id: 'option.measurementUnits.feet',
+        defaultMessage: 'feet',
+      },
+      grams: {
+        id: 'option.measurementUnits.grams',
+        defaultMessage: 'grams',
+      },
+      inches: {
+        id: 'option.measurementUnits.inches',
+        defaultMessage: 'inches',
+      },
+      kilograms: {
+        id: 'option.measurementUnits.kilograms',
+        defaultMessage: 'kilograms',
+      },
+      liters: {
+        id: 'option.measurementUnits.liters',
+        defaultMessage: 'liters',
+      },
+      millimeters: {
+        id: 'option.measurementUnits.millimeters',
+        defaultMessage: 'millimeters',
+      },
+      meters: {
+        id: 'option.measurementUnits.meters',
+        defaultMessage: 'meters',
+      },
+      minutes: {
+        id: 'option.measurementUnits.minutes',
+        defaultMessage: 'minutes',
+      },
+      pixels: {
+        id: 'option.measurementUnits.pixels',
+        defaultMessage: 'pixels',
+      },
+      pounds: {
+        id: 'option.measurementUnits.pounds',
+        defaultMessage: 'pounds',
+      },
+      'square-feet': {
+        id: 'option.measurementUnits.square-feet',
+        defaultMessage: 'square feet',
+      },
+      stories: {
+        id: 'option.measurementUnits.stories',
+        defaultMessage: 'stories',
+      },
+    }),
+  },
+};
