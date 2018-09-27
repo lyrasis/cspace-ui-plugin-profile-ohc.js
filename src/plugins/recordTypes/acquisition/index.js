4348394cd6cbd504c8e3dea6d5b0a49c22f427f8
Richard Millet <remillet@yahoo.com>
2018-09-27 14:06:47 -0700

    DRYD-485: Updates to acquisition UI layout.

---
+++ src/plugins/recordTypes/acquisition/index.js
@@ -0,0 +1,11 @@
+import optionLists from './optionLists';
+import forms from './forms';
+
+export default () => configContext => ({
+  optionLists,
+  recordTypes: {
+    acquisition: {
+      forms: forms(configContext),
+    },
+  },
+});
--- src/plugins/recordTypes/index.js
+++ src/plugins/recordTypes/index.js
@@ -1,5 +1,8 @@
 import collectionobject from './collectionobject';
+import acquisition from './acquisition';
+
 
 export default [
   collectionobject,
+  acquisition,
 ];
