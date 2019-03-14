677955f62d0687c67e31f1649dd5a171baefd82d
Brian Harrington <brian.harrington@lyrasis.org>
2019-03-14 09:03:45 -0400

    Some fixes for OHC

---
+++ src/plugins/recordTypes/conditioncheck/optionLists.js
@@ -0,0 +1,60 @@
+import { defineMessages } from 'react-intl';
+
+export default {
+  conditionCheckReasons: {
+    values: [
+      'candidate for deaccession',
+      'conservation',
+      'consideration',
+      'damaged',
+      'exhibit',
+      'inquiry',
+      'exhibit preparation',
+      'inventory/recataloging',
+      'loanin',
+      'loanout',
+    ],
+    messages: defineMessages({
+      'candidate for deaccession': {
+        id: 'option.conditionCheckReasons.candidate for deaccession',
+        defaultMessage: 'candidate for deaccession',
+      },
+      conservation: {
+        id: 'option.conditionCheckReasons.conservation',
+        defaultMessage: 'conservation',
+      },
+      consideraton: {
+        id: 'option.conditionCheckReasons.consideration',
+        defaultMessage: 'consideration',
+      },
+      damaged: {
+        id: 'option.conditionCheckReasons.damaged',
+        defaultMessage: 'damaged',
+      },
+      exhibit: {
+        id: 'option.conditionCheckReasons.exhibit',
+        defaultMessage: 'exhibit',
+      },
+      'exhibit preparation': {
+        id: 'option.conditionCheckReasons.exhibit preparation',
+        defaultMessage: 'exhibit preparation',
+      },
+      inquiry: {
+        id: 'option.conditionCheckReasons.inquiry',
+        defaultMessage: 'inquiry',
+      },
+      'inventory/recataloging': {
+        id: 'option.conditionCheckReasons.inventory/recataloging',
+        defaultMessage: 'inventory/recataloging',
+      },
+      loanin: {
+        id: 'option.conditionCheckReasons.loanin',
+        defaultMessage: 'loan in',
+      },
+      loanout: {
+        id: 'option.conditionCheckReasons.loanout',
+        defaultMessage: 'loan out',
+      },
+    }),
+  },
+}
