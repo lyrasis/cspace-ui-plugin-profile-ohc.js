f3d16da29dfcb61f16c88785af1e63e89545548b
Ray Lee <ray.lee@lyrasis.org>
2019-05-24 23:29:08 -0700

    DRYD-655: Upgrade to babel 7 and webpack 4.

--- src/plugins/extensions/index.js
+++ src/plugins/extensions/index.js
@@ -2,10 +2,8 @@ import dimension from './dimension';
 import annotation from './annotation';
 import naturalhistory from './naturalhistory';
 
-
 export default [
   dimension,
   annotation,
   naturalhistory,
 ];
-
