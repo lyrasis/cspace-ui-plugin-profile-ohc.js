535258007f674ae717fd9a1d8c5eba5ec0d76f9c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-12-16 16:13:37 -0500

    Add terms to personTitles optionList

--- src/plugins/recordTypes/index.js
+++ src/plugins/recordTypes/index.js
@@ -3,6 +3,7 @@ import conditioncheck from './conditioncheck';
 import acquisition from './acquisition';
 import intake from './intake';
 import objectexit from './objectexit';
+import person from './person';
 
 export default [
   collectionobject,
@@ -10,4 +11,5 @@ export default [
   acquisition,
   intake,
   objectexit,
+  person,
 ];
