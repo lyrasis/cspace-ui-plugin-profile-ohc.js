8dc8a91c511abee8939c4d884f05bf74853106b6
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-06-06 15:23:20 -0400

    Override movement optionLists and customize moveMethods

---
+++ src/plugins/recordTypes/movement/optionLists.js
@@ -0,0 +1,90 @@
+import { defineMessages } from 'react-intl';
+
+export default {
+  moveMethods: {
+    values: [
+      '40" textile box',
+      '60" textile box',
+      'bin box',
+      'blanket wrapped',
+      'crate',
+      'custom box',
+      'drawer',
+      'fragile',
+      'garment rack',
+      'handcarried',
+      'hazardous',
+      'heavy',
+      "large banker's box",
+      'pallet',
+      "small banker's box",
+      'speed pack',
+    ],
+    messages: defineMessages({
+      '40" textile box': {
+        id: 'option.moveMethods.40" textile box',
+        defaultMessage: '40" textile box',
+      },
+      '60" textile box': {
+        id: 'option.moveMethods.60" textile box',
+        defaultMessage: '60" textile box',
+      },
+      'bin box': {
+        id: 'option.moveMethods.bin box',
+        defaultMessage: 'bin box',
+      },
+      'blanket wrapped': {
+        id: 'option.moveMethods.blanket wrapped',
+        defaultMessage: 'blanket wrapped',
+      },
+      crate: {
+        id: 'option.moveMethods.crate',
+        defaultMessage: 'crate',
+      },
+      'custom box': {
+        id: 'option.moveMethods.custom box',
+        defaultMessage: 'custom box',
+      },
+      drawer: {
+        id: 'option.moveMethods.drawer',
+        defaultMessage: 'drawer',
+      },
+      fragile: {
+        id: 'option.moveMethods.fragile',
+        defaultMessage: 'fragile',
+      },
+      'garment rack': {
+        id: 'option.moveMethods.garment rack',
+        defaultMessage: 'garment rack',
+      },
+      handcarried: {
+        id: 'option.moveMethods.handcarried',
+        defaultMessage: 'hand carry',
+      },
+      hazardous: {
+        id: 'option.moveMethods.hazardous',
+        defaultMessage: 'hazardous',
+      },
+      heavy: {
+        id: 'option.moveMethods.heavy',
+        defaultMessage: 'heavy',
+      },
+      "large banker's box": {
+        id: "option.moveMethods.large banker's box",
+        defaultMessage: "large banker's box",
+      },
+      pallet: {
+        id: 'option.moveMethods.pallet',
+        defaultMessage: 'pallet',
+      },
+      "small banker's box": {
+        id: "option.moveMethods.small banker's box",
+        defaultMessage: "small banker's box",
+      },
+      'speed pack': {
+        id: 'option.moveMethods.speed pack',
+        defaultMessage: 'speed pack',
+      },
+    }),
+  },
+};
